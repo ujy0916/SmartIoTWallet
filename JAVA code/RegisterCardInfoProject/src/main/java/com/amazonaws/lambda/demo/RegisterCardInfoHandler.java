bd7af6a980a0f05d945666c71965ad14719ddbf5
yangyu <did931@naver.com>
2020-11-30 15:43:22 +0900

    upload java code
    upload java code (11/27,28)

---
+++ JAVA code/RegisterCardInfoProject/src/main/java/com/amazonaws/lambda/demo/RegisterCardInfoHandler.java
@@ -0,0 +1,83 @@
+package com.amazonaws.lambda.demo;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+import com.amazonaws.lambda.demo.Thing.State;
+import com.amazonaws.lambda.demo.Thing.State.Tag;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class RegisterCardInfoHandler implements RequestHandler<Document, String> {
+    private DynamoDB dynamoDb;
+    private String DYNAMODB_TABLE_NAME = "Card_value";
+
+    @Override
+    public String handleRequest(Document input, Context context) {
+        this.initDynamoDbClient();
+        context.getLogger().log("Input: " + input);
+
+        //return null;
+        return persistData(input);
+    }
+
+    private String persistData(Document document) throws ConditionalCheckFailedException {
+
+        // Epoch Conversion Code: https://www.epochconverter.com/
+        SimpleDateFormat sdf = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+        String timeString = sdf.format(new java.util.Date (document.timestamp*1000));
+        //*1000한 이유: 1초는 1000ms이기 때문.
+
+        /*// temperature와 LED 값이 이전상태와 동일한 경우 테이블에 저장하지 않고 종료 
+        if (document.current.state.reported.Cardrfid.equals(document.previous.state.reported.Cardrfid) && 
+                document.current.state.reported.CardName.equals(document.previous.state.reported.CardName)) {
+                return null;
+        }*/
+
+        return this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+                .putItem(new PutItemSpec().withItem(new Item().withPrimaryKey("Card_rfid", document.current.state.reported.Card_rfid)
+                		//.withLong("time", document.timestamp)
+                        //.withString("temperature", document.current.state.reported.temperature)
+                        //.withString("LED", document.current.state.reported.LED)
+                		.withString("Card_name",document.current.state.reported.Card_name)
+                        .withString("timestamp",timeString)))
+                .toString();
+    }
+
+    private void initDynamoDbClient() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion("ap-northeast-2").build();
+
+        this.dynamoDb = new DynamoDB(client);
+    }
+
+}
+
+class Document {
+    public Thing previous;       
+    public Thing current;
+    public long timestamp;
+    //public String device;       // AWS IoT에 등록된 사물 이름 
+}
+
+class Thing {
+    public State state = new State();
+    public long timestamp;
+    public String clientToken;
+
+    public class State {
+        public Tag reported = new Tag();
+        public Tag desired = new Tag();
+
+        public class Tag {
+            public String Card_rfid;
+            public String Card_name;
+        }
+    }
+}
---
+++ JAVA code/UploadRFIDValueLambdaJavaProject/src/main/java/com/amazonaws/lambda/demo/UploadRFIDValueHandler.java
@@ -0,0 +1,97 @@
+package com.amazonaws.lambda.demo;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/*
+ * DynamoDB(RFID_Value)에 RFID와 LED, Timestamp 값을 올려주는 람다함수.
+ */
+
+public class UploadRFIDValueHandler implements RequestHandler<Document, String> {
+    private DynamoDB dynamoDb;
+    private String DYNAMODB_TABLE_NAME = "RFID_Value";
+
+    @Override
+    public String handleRequest(Document input, Context context) {
+        this.initDynamoDbClient();
+        context.getLogger().log("Input: " + input);
+
+        //return null;
+        return persistData(input);
+    }
+
+    private String persistData(Document document) throws ConditionalCheckFailedException {
+
+        // Epoch Conversion Code: https://www.epochconverter.com/
+        SimpleDateFormat sdf = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+        String timeString = sdf.format(new java.util.Date (document.timestamp*1000));
+
+        // RFID와 LED 값이 이전상태와 동일한 경우 테이블에 저장하지 않고 종료 
+        if (document.current.state.reported.RFID.equals(document.previous.state.reported.RFID) && 
+                document.current.state.reported.State.equals(document.previous.state.reported.State)) {
+                return null;
+        }
+        String state = "";
+        if(document.current.state.reported.State.equals("1")) {state = "OPEN";}
+        else if(document.current.state.reported.State.equals("0")) {state = "CLOSE";}
+
+        return this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+                .putItem(new PutItemSpec().withItem(new Item().withPrimaryKey("RFID", document.current.state.reported.RFID)
+                        .withLong("time", document.timestamp)
+                        .withString("State", state)
+                        .withString("timestamp",timeString)))
+                .toString();
+    }
+
+    private void initDynamoDbClient() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion("ap-northeast-2").build();
+
+        this.dynamoDb = new DynamoDB(client);
+    }
+
+}
+
+/**
+ * AWS IoT은(는) 섀도우 업데이트가 성공적으로 완료될 때마다 /update/documents 주제에 다음 상태문서를 게시합니다
+ * JSON 형식의 상태문서는 2개의 기본 노드를 포함합니다. previous 및 current. 
+ * previous 노드에는 업데이트가 수행되기 전의 전체 섀도우 문서의 내용이 포함되고, 
+ * current에는 업데이트가 성공적으로 적용된 후의 전체 섀도우 문서가 포함됩니다. 
+ * 섀도우가 처음 업데이트(생성)되면 previous 노드에는 null이 포함됩니다.
+ * 
+ * timestamp는 상태문서가 생성된 시간 정보이고, device는 상태문서에 포함된 값이 아닌 Iot규칙을 통해서 Lambda함수로 전달된 값이다. 
+ * 이 값을 해당 규칙과 관련된 사물이름을 나타낸다. 
+ */
+class Document {
+    public Thing previous;       
+    public Thing current;
+    public long timestamp;
+    public String device;       // AWS IoT에 등록된 사물 이름 
+    //public String RFID;
+}
+
+class Thing {
+    public State state = new State();
+    public long timestamp;
+    public String clientToken;
+    //public String RFID;
+
+    public class State {
+        public Tag reported = new Tag();
+        public Tag desired = new Tag();
+
+        public class Tag {
+            public String RFID;
+            public String State;
+        }
+    }
+}
