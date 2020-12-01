4ffaeda33a54958f8caa5dc92085c9269ab1a643
ChoiJE <wjddms8031@naver.com>
2020-12-01 16:30:14 +0900

    Upload_DB값가져오기

--- JAVA code/GetRFIDInfoFunctionHandler.java
+++ JAVA code/GetRFIDInfoFunctionHandler.java
@@ -1,7 +1,10 @@
 package com.amazonaws.lambda.demo;
 
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
+import java.util.Iterator;
 
 import com.amazonaws.lambda.demo.Thing.State;
 import com.amazonaws.lambda.demo.Thing.State.Tag;
@@ -12,8 +15,12 @@ import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
 
 public class GetRFIDInfoFunctionHandler implements RequestHandler<Document, String> {
 
@@ -24,12 +31,17 @@ public class GetRFIDInfoFunctionHandler implements RequestHandler<Document, Stri
     public String handleRequest(Document input, Context context) {
         this.initDynamoDbClient();
         context.getLogger().log("Input: " + input);
+        
+        persistData(input);
+        
+        return "end";
+        
+        //return persistData();
 
-        //return null;
-        return persistData(input);
+        //return persistData(input);
     }
 
-    private String persistData(Document document) throws ConditionalCheckFailedException {
+    private void persistData(Document document) throws ConditionalCheckFailedException {
 
         // Epoch Conversion Code: https://www.epochconverter.com/
         SimpleDateFormat sdf = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
@@ -37,12 +49,31 @@ public class GetRFIDInfoFunctionHandler implements RequestHandler<Document, Stri
         String timeString = sdf.format(new java.util.Date (document.timestamp*1000));
         //*1000한 이유: 1초는 1000ms이기 때문.
         
-        return this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+        Table table = dynamoDb.getTable(DYNAMODB_TABLE_NAME);
+        
+        Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
+        expressionAttributeValues.put(":id", 0);
+        
+        ItemCollection<ScanOutcome> items = table.scan("id > :id",
+        		"firstName, lastName",
+        		null,
+        		expressionAttributeValues);
+        
+        Iterator<Item> iterator = items.iterator();
+        while (iterator.hasNext()) {
+        	System.out.println(iterator.next().toJSONPretty());
+        }
+        
+         
+        
+        /*테이블에서 항목 하나 뽑아오기
+         * return this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
         		.getItem(new GetItemSpec().withPrimaryKey("id", 1)
         				.withProjectionExpression("id, firstName, lastName")
-        				.withConsistentRead(true)).toString();
+        				.withConsistentRead(true)).toString();*/
 
-        /*return this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+        /*테이블에 저장
+         * return this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
                 .putItem(new PutItemSpec().withItem(new Item().withPrimaryKey("Card_rfid", document.current.state.reported.Card_rfid)
                 		.withString("Card_name",document.current.state.reported.Card_name)
                         .withString("timestamp",timeString)))
