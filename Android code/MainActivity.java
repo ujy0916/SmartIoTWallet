200ea5ee114827cbd80ad3230fd2300d2c5e4e28
yangyu <did931@naver.com>
2020-12-05 00:43:56 +0900

    upload 일요일에 할 것들
    1. RegisterCardActivity.java
    CardFreqActivity.java
    완성하기
    
    2. 비활성화 모드
    
    3. BLE-안드로이드 통신

--- Android code/MainActivity.java
+++ Android code/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jung.android.smartiotwalletapp.ui;
+package com.hansung.android.smartiotwalletapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,21 +7,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.jung.android.smartiotwalletapp.R;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-//import com.jung.android.smartiotwalletapp.ui.apicall.GetCards;
+import com.hansung.android.smartiotwalletapp.R;
 
 public class MainActivity extends AppCompatActivity {
     final static String TAG = "AndroidAPITest";
     String getCardNameUrl;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //카드 등록
+        Button registerCardBtn = findViewById(R.id.registerCardBtn);
+        registerCardBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getCardNameUrl = "";
+                Log.i(TAG, "getCardNameUrl=" + getCardNameUrl);
+                if (getCardNameUrl == null || getCardNameUrl.equals("")) {
+                    Toast.makeText(MainActivity.this, "카드 조회 API URI가 설정되어 있지 않습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(MainActivity.this, RegisterCardActivity.class);
+                //intent.putExtra("getCardNameUrl", getCardNameUrl);
+                startActivity(intent);
+                //MainActivity->CardListActivity->GetTings(extends, GetResquest)
+            }
+        });
+
+        //카드 조회
         Button listCardBtn = findViewById(R.id.listCardBtn);
         listCardBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -38,8 +52,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
+        //카드 빈도수 조회
+        Button freqCardBtn = findViewById(R.id.freqCardBtn);
+        freqCardBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getCardNameUrl = "";
+                Log.i(TAG, "getCardNameUrl=" + getCardNameUrl);
+                if (getCardNameUrl == null || getCardNameUrl.equals("")) {
+                    Toast.makeText(MainActivity.this, "카드 조회 API URI가 설정되어 있지 않습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(MainActivity.this, CardFreqActivity.class);
+                //intent.putExtra("getCardNameUrl", getCardNameUrl);
+                startActivity(intent);
+                //MainActivity->CardListActivity->GetTings(extends, GetResquest)
+            }
+        });
 
     }
 }
---
+++ Android code/RegisterCardActivity.java
@@ -0,0 +1,17 @@
+package com.hansung.android.smartiotwalletapp.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hansung.android.smartiotwalletapp.R;
+
+public class RegisterCardActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_card);
+
+
+    }
+
+}
