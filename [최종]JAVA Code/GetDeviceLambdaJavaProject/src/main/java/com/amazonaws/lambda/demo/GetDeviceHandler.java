2a6eeb1624ad7d8c196c60918cb3f3b282742eb0
ChoiJE <wjddms8031@naver.com>
2020-12-10 10:51:00 +0900

    Create GetDeviceHandler(GetDeviceFunction)
    /devices/{device} - GET - 메서드 실행

---
+++ [최종]JAVA Code/GetDeviceLambdaJavaProject/src/main/java/com/amazonaws/lambda/demo/GetDeviceHandler.java
@@ -0,0 +1,31 @@
+package com.amazonaws.lambda.demo;
+
+import com.amazonaws.services.iotdata.AWSIotData;
+import com.amazonaws.services.iotdata.AWSIotDataClientBuilder;
+import com.amazonaws.services.iotdata.model.GetThingShadowRequest;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/*
+ * GetDeviceFunction
+ * /devices/{device} - GET - 메서드 실행
+ */
+public class GetDeviceHandler implements RequestHandler<Event, String> {
+
+    @Override
+    public String handleRequest(Event event, Context context) {
+        AWSIotData iotData = AWSIotDataClientBuilder.standard().build();
+
+        GetThingShadowRequest getThingShadowRequest  = 
+        new GetThingShadowRequest()
+            .withThingName(event.device);
+
+        iotData.getThingShadow(getThingShadowRequest);
+
+        return new String(iotData.getThingShadow(getThingShadowRequest).getPayload().array());
+    }
+}
+
+class Event {
+    public String device;
+}
