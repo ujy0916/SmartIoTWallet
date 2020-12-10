0f128b0649fce6484a20608a59ce2c3f1c34f5c8
ChoiJE <wjddms8031@naver.com>
2020-12-10 09:31:23 +0900

    Create AWS_IoT_RFID.ino
    MKRWiFi1010 보드를 이용하여 원하는 RFID 값을 시리얼 모니터를 통해 선택하여 전송해줌

---
+++ [최종]Arduino Code/AWS_IoT_RFID.ino
@@ -0,0 +1,214 @@
+#include <ArduinoBearSSL.h>
+#include <ArduinoECCX08.h>
+#include <ArduinoMqttClient.h>
+#include <WiFiNINA.h> // change to #include <WiFi101.h> for MKR1000
+
+#include "arduino_secrets.h"
+#define btn           11 //버튼
+
+
+int state = 1; //지갑 상태(OPEN/CLOSE)
+int control = 1; //1은 활성화, 0은 비활성화
+int dec = 0; //rfid 값
+
+int rfid1 = 253832; //네이버페이
+int rfid2 = 247213677; //카카오뱅크
+int rfid3 = 183177411; //신한신용
+int rfid4 = 21116512; //국민체크
+int rfid5 =21511121; //K뱅크   
+
+#include <ArduinoJson.h>
+
+///////arduino_secrets.h에 개인 정보 입력
+const char ssid[]        = SECRET_SSID;
+const char pass[]        = SECRET_PASS;
+const char broker[]      = SECRET_BROKER;
+const char* certificate  = SECRET_CERTIFICATE;
+
+WiFiClient    wifiClient;            // Used for the TCP socket connection
+BearSSLClient sslClient(wifiClient); // Used for SSL/TLS connection, integrates with ECC508
+MqttClient    mqttClient(sslClient);
+
+void setup() {
+  Serial.begin(115200);
+  delay(5000);
+  while (!Serial);
+
+  pinMode(btn, INPUT); //버튼
+
+
+  if (!ECCX08.begin()) {
+    Serial.println("No ECCX08 present!");
+    while (1);
+  }
+
+  ArduinoBearSSL.onGetTime(getTime);
+
+  sslClient.setEccSlot(0, certificate);
+
+  mqttClient.onMessage(onMessageReceived);
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+
+  if (!mqttClient.connected()) {
+    // MQTT client is disconnected, connect
+    connectMQTT();
+  }
+
+  // poll for new MQTT messages and send keep alives
+  mqttClient.poll();
+
+   char payload[512];
+
+  //버튼 
+    char i;
+    if(digitalRead(btn) == HIGH){state = 1;} //OPEN
+    else if(digitalRead(btn) == LOW){
+      state = 0;
+      getRFID_Status(payload, '6');
+    }
+    
+    if (Serial.available() > 0) {
+      
+      i = Serial.read();
+      
+      getRFID_Status(payload, i);
+  }
+}
+
+unsigned long getTime() {
+  //WiFi 모듈로 부터 현재 시간 받아옴  
+  return WiFi.getTime();
+}
+
+void connectWiFi() {
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.print(ssid);
+  Serial.print(" ");
+
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    //실패할 경우, 재시도
+    Serial.print(".");
+    delay(5000);
+  }
+  Serial.println();
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+}
+
+void connectMQTT() {
+  Serial.print("Attempting to MQTT broker: ");
+  Serial.print(broker);
+  Serial.println(" ");
+
+  while (!mqttClient.connect(broker, 8883)) {
+    //실패할 경우, 재시도
+    Serial.print(".");
+    delay(5000);
+  }
+  Serial.println();
+
+  Serial.println("You're connected to the MQTT broker");
+  Serial.println();
+
+  //주제 구독
+  mqttClient.subscribe("$aws/things/MyMKRWiFi1010/shadow/update/delta");
+}
+
+void getRFID_Status(char* payload, char i) {
+  // rfid가 비활성화 모드일 경우, 카드를 읽지 않도록 한다.
+  if ( control == 0 ) {
+    Serial.print("[비활성]control: ");Serial.println(control);
+    return;
+  }
+
+  else{
+    Serial.print("[활성]control: ");Serial.println(control);
+    if(state == 0){ //지갑이 닫혀있는 상황일 때, 지갑이 닫혔음을 알려줌.
+       dec=0;
+       sprintf(payload,"{\"state\":{\"reported\":{\"RFID\":\"%d\",\"State\":\"%d\"}}}",dec,state);
+       delay(10000);
+     }
+    else{
+    switch(i){
+      case '1': dec=rfid1; break;
+      case '2': dec=rfid2; break;
+      case '3': dec=rfid3; break;
+      case '4': dec=rfid4; break;
+      case '5': dec=rfid5; break;
+      } 
+      sprintf(payload,"{\"state\":{\"reported\":{\"RFID\":\"%d\",\"State\":\"%d\"}}}",dec,state);
+     }
+     sendMessage(payload);
+  }
+}
+
+
+void sendMessage(char* payload) {
+  char TOPIC_NAME[]= "$aws/things/MyMKRWiFi1010/shadow/update";
+  
+  Serial.print("Publishing send message:");
+  Serial.println(payload);
+  mqttClient.beginMessage(TOPIC_NAME);
+  mqttClient.print(payload);
+  mqttClient.endMessage();
+}
+
+
+void onMessageReceived(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.print("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  // store the message received to the buffer
+  char buffer[512] ;
+  int count=0;
+  while (mqttClient.available()) {
+     buffer[count++] = (char)mqttClient.read();
+  }
+  buffer[count]='\0'; // 버퍼의 마지막에 null 캐릭터 삽입
+  Serial.println(buffer);
+  Serial.println();
+  
+  // JSon 형식의 문자열인 buffer를 파싱하여 필요한 값을 얻어옴.
+  // 디바이스가 구독한 토픽이 $aws/things/MyMKRWiFi1010/shadow/update/delta 이므로,
+  // JSon 문자열 형식은 다음과 같다.
+  // {
+  //    "version":391,
+  //    "timestamp":1572784097,
+  //    "state":{
+  //        "LED":"ON" -->여기부분에 DISABLED 삽입
+  //    },
+  //    "metadata":{
+  //        "LED":{
+  //          "timestamp":15727840
+  //         }
+  //    }
+  // }
+  //
+  
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, buffer);
+  JsonObject root = doc.as<JsonObject>();
+  JsonObject state = root["state"];
+  const char* DisAbled = state["DISABLED"];
+  
+  char payload[512];
+
+  if (strcmp(DisAbled,"abled")==0) {
+    Serial.print("DISABLED:");Serial.println(DisAbled);
+    control = 1;    
+  }
+  else {
+    Serial.print("DISABLED:");Serial.println(DisAbled);
+    control = 0;
+  }
+}
